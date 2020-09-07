b169513ac5ead166e7b3a40bb91f16c78477c718
Aguspy <janifu02@gmail.com>
2020-09-07 14:05:23 +0200

    initial commit

--- src/mainPackage/SchabernackMain.java
+++ src/mainPackage/SchabernackMain.java
@@ -1,9 +1,12 @@
 package mainPackage;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 public class SchabernackMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.firefox.driver", "/Schabernack/dependencies/geckodriver");
+		WebDriver driver = new FirefoxDriver();
 		System.out.println("schabernack");
 	}
 
